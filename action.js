21faca565290a4e3c7ab4bab3a37a6f22a46324c
Vaishak Maharaj <68328397+VaishakM2001@users.noreply.github.com>
2021-05-18 20:08:32 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function but_1()
+{
+    document.body.style.background="linear-gradient(to right, #64f38c, #f79d00)"
+}
+function but_2()
+{
+    document.body.style.background="linear-gradient(to right, #cb2d3e, #ef473a)"
+}
+function but_3()
+{
+    document.body.style.background="linear-gradient(to right, #000428, #004e92)"
+}
